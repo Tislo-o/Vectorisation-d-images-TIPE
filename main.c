8c80f7124bff93997a85c53fd8680d7a8d3c173c
Tislo-o <musictitwan@gmail.com>
2026-03-02 18:43:15 +0100

    Refactor SVG creation and update point structures

--- main.c
+++ main.c
@@ -3,53 +3,79 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+
 #include "r_string.h"
 
 #define N 3
-typedef struct point {
-    float x;
-    float y;
-}point_t;
+
+typedef struct{
+    uint16_t x;
+    uint16_t y;
+}Pixel;
+
+typedef struct{
+    int width;
+    int height;
+    int channels; //nombre de canaux, 3 ou 4 suivant la présence du canal alpha
+    uint8_t* data;
+}bitmap_img;
+
+typedef struct{
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+}Color;
+
+//Renvoie la couleur du pixel p dans l'image bitmap img
+Color pixel_color(bitmap_img* img, Pixel* p) {
+    assert(p->x >= 0 && p->y >= 0 && p->x < img->width && p->y < img->height);
+
+    int idx = (p->y * img->width + p->x) * img->channels;
+
+    return (Color){img->data[idx], img->data[idx + 1], img->data[idx + 2]};
+}
 
 //à partir d'une liste de points, construit un chaîne de caractère dans str
 //décrivant le path passant par tous les points
-r_string* create_path(const point_t* points, int n_points) {
+r_string* create_path(const Pixel* pixels, int n_points) {
     assert(n_points >= 3);
 
     r_string* s = create_r_string("M");
 
     int i = 0; // indice courant dans la tableau de points
 
-    concat_float(s, points[0].x);
-    concat_float(s, points[0].y);
+    concat_float(s, (float)pixels[0].x);
+    concat_float(s, (float)pixels[0].y);
     while (i + 2 < n_points) {
         concat_str(s, " Q");
-        concat_float(s, 2*points[i+1].x - points[i].x / 2 - points[i+2].x / 2);
-        concat_float(s, 2*points[i+1].y - points[i].y / 2 - points[i+2].y / 2);
-        concat_float(s, points[i+2].x);
-        concat_float(s, points[i+2].y);
+        concat_float(s, 2.f * (float)pixels[i+1].x - (float)(pixels[i].x + pixels[i+2].x) / 2.f);
+        concat_float(s, 2.f * (float)pixels[i+1].y - (float)(pixels[i].y + pixels[i+2].y) / 2.f);
+        concat_float(s, (float)pixels[i+2].x);
+        concat_float(s, (float)pixels[i+2].y);
         i += 2;
     }
-    if (i + 2 == n_points) { //si le point d'indice i + 1 n'est relié à personne
-        concat_str(s, " Q");   //on referme la forme avec une courbe jusqu'au point de départ
-        concat_float(s, 2*points[i+1].x - points[i].x / 2 - points[0].x / 2);
-        concat_float(s, 2*points[i+1].y - points[i].y / 2 - points[0].y / 2);
-        concat_float(s, points[0].x);
-        concat_float(s, points[0].y);
+    if (i + 2 == n_points) { //si le Pixel d'indice i + 1 n'est relié à personne
+        concat_str(s, " Q");   //on referme la forme avec une courbe jusqu'au Pixel de départ
+        concat_float(s, 2.f * (float)pixels[i+1].x - (float)(pixels[i].x + pixels[0].x) / 2.f);
+        concat_float(s, 2.f * (float)pixels[i+1].y - (float)(pixels[i].y + pixels[0].y) / 2.f);
+        concat_float(s, (float)pixels[0].x);
+        concat_float(s, (float)pixels[0].y);
     } else { //tous les points ont été reliés
-        concat_str(s, " Z"); //on referme la forme par une droite jusqu'au point de départ
+        concat_str(s, " Z"); //on referme la forme par une droite jusqu'au Pixel de départ
     }
 }
 
+
+
 //crée un fichier svg de nom 'filename' suivant les dimensions 'width' et 'height'
-void create_svg(const char* filename, int width, int height) {
+void create_svg(const char* filename) {
     FILE* f = fopen(filename, "w");
     //Ecriture du header
     fprintf(f, " <svg xmlns=\"http://www.w3.org/2000/svg\" ");
-    fprintf(f, " width=\"%dpx\" height=\"%dpx\" ", width, height);
+    fprintf(f, " width=\"%dpx\" height=\"%dpx\" ", 2000, 2000);
     fprintf(f, " viewBox=\"0 0 %d %d\"> ", 50 , 50);
 
-    point_t pt[N] = {{2.f,2.f}, {46.f,16.f}, {25.f, 26.f}};
+    Pixel pt[N] = {{2.f,2.f}, {46.f,16.f}, {25.f, 26.f}};
     r_string* path = create_path(pt, N);
 
     fprintf(f, "<path d=\"%s\" fill=\"#b8ddf0\"/>", path->data);
@@ -57,7 +83,7 @@ void create_svg(const char* filename, int width, int height) {
 
     //optionnel: dessiner les points
     for (int i = 0; i < N; ++i) {
-        fprintf(f, "<circle cx=\"%f\" cy=\"%f\" r=\"1\" fill=\"#d400ff\"/>", pt[i].x, pt[i].y);
+        fprintf(f, "<circle cx=\"%d\" cy=\"%d\" r=\"1\" fill=\"#d400ff\"/>", pt[i].x, pt[i].y);
     }
     fprintf(f, "</svg>");
     fclose(f);
@@ -65,16 +91,19 @@ void create_svg(const char* filename, int width, int height) {
 
 int main() {
     /***  CODE POUR LIRE UNE IMAGE ***/
-    // int x,y,n;
-    // unsigned char *data = stbi_load("paysage.jpg", &x, &y, &n, 0);
-    // printf("largeur: %d   hauteur: %d   nombre de composantes: %d", x, y, n);
-    // if (data == NULL) {
+    // bitmap_img img; 
+    // img.data = stbi_load("paysage.jpg", &img.width, &img.height, &img.channels, 0);
+    // if (img.data == NULL) {
     //     printf("Erreur chargement\n");
     //     return 1;
     // }
-    // stbi_image_free(data);
+    // for (int i = 0; i < img.height; ++i) {
+    //     for (int j = 0; j < img.width; ++j) {
+    //     }
+    // }
+    // stbi_image_free(img.data);  
 
-    create_svg("resultat.svg", 2000, 2000);
+    create_svg("resultat.svg");
     return 0;
 }
 
