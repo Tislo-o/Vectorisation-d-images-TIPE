93a1dc373ba7fee055e51748dbbe1690b4aff295
Tislo-o <musictitwan@gmail.com>
2026-03-30 21:23:52 +0200

    Changement de méthode avec tableaux next et prev

--- dense_set.h
+++ dense_set.h
@@ -2,61 +2,76 @@
 #include <assert.h>
 #include <stdbool.h>
 #include "global.h"
-#define NONE_U32 ~((u32)0)
+#define NONE ~((u32)0)
+#define REMOVED (NONE-1)
 
-//SDA : ensemble d'entier naturels
+//array of distinct elements which provides these operations in O(1) time:
+//-know if an element is in the array
+//-remove an element by value
+//-get the minimum element of the array
 typedef struct dense_set{
-    u32* data;
-    u32 count; //nombre d'élément
-}dset;
+    u32 count;  //number of elements remaining
+    u32 min;    //minimum of the set
+    u32 allocated; //number of elements allocated
+    u32* next;  //next[i] can equal... 
+                //k: the element after i is k
+                //NONE:    no element after i, i is the maximum
 
-/*Renvoie un pointeur vers un dset = {0, 1, ..., count-1} .*/
+    u32* prev;  //prev[i] can equal... 
+                //k: the element before i is k
+                //NONE:    no element before i, i is the minimum
+                //REMOVED:    i was removed from the set
+}dset;
+//return a dset with containing all the integers between 0 and count-1
 dset* full_dset(u32 count) {
-    dset* d = malloc(sizeof(dset) * count);
-    d->data = malloc(sizeof(u32) * count);
+    dset* d = malloc(sizeof(dset));
+    d->prev = malloc(sizeof(u32) * count);
+    d->next = malloc(sizeof(u32) * count);
     d->count = count;
+    d->allocated = count;
+    d->min = 0;
     for (int i = 0; i < count; ++i) {
-        d->data[i] = 0;
+        d->prev[i] = i-1;
+        d->next[i] = i+1;
     }
+    d->prev[0] = NONE;
+    d->next[count-1] = NONE;
+
     return d;
 }
-
-/*Libère la mémoire de d.*/
 void free_dset(dset* d) {
-    free(d->data);
+    free(d->prev);
+    free(d->next);
     free(d);
 }
 
-u32 dset_give_ind_min(u32* data, u32 acc) {
-    u32 i = data[acc];
-    assert(i != NONE_U32);
-    if (i == 0) {
-        return acc;
-    }else{
-        return dset_give_ind_min(data, acc + i);
-    }
-}
-
-/*Renvoie le min de d.*/
-u32 dset_give_element(dset* d) {
+u32 dset_give_min_element(dset* d) {
     assert(d->count != 0);
-    u32 i = dset_give_ind_min(d->data, 0);
-    data[0] = i; //modifie le tableau pour que le prochain appel soit plus rapide.
-    return i;
+    return d->min;
 }
-
-/*Renvoie true si e est dans d sinon renvoie false.*/
+//return if e is in the dset, e must be 
 bool is_in_dset(dset* d, u32 e) {
-    return d->indices[e] == 0;
+    assert(e < d->allocated);
+    return d->prev[e] != REMOVED;
 }
-
-/*Supprime e de d.*/
+//remove an element that must be in the dset
 void dset_remove_element(dset* d, u32 e) {
-    u32* data = d->data;
-    if (e == d->count - 1){
-        data[e] = NONE_U32;
-    }else{
-        data[e] = data[e + 1] + 1;
+    assert(e < d->allocated);
+
+    u32 p = d->prev[e];
+    u32 n = d->next[e];
+
+    assert(p != REMOVED);
+
+    if (p != NONE) { 
+        d->next[p] = n;
+    } else {        //e is the minimum
+        d->min = n;
     }
+    if (n != NONE) {
+        d->prev[n] = p;
+    }
+
+    d->prev[e] = REMOVED;
     d->count--;
 }
